b0005ac573babc0c6c41d5bcf97c25d00c3f230f
Andrew Skurlatov <a.skurlatov@gmail.com>
2022-08-09 13:47:57 +0200

    feat: logger as a singltone

--- logger/logger.go
+++ logger/logger.go
@@ -1,42 +1,29 @@
 package logger
 
 import (
-	"fmt"
+	"sync"
 
 	"github.com/sirupsen/logrus"
 )
 
-// ILogger represent microservice application
-// standard logger interface
-type ILogger interface {
-	logrus.FieldLogger
-	SetLevel(level string) error
-}
-
-// Logger represent standard logger structure
-type Logger struct {
+// logger represent standard logger structure
+type logger struct {
 	*logrus.Logger
 }
 
-// NewLogger create new logger instance
-func NewLogger() ILogger {
-	logger := logrus.New()
-	formatter := &logrus.TextFormatter{
-		FullTimestamp: true,
-	}
+var instance *logger
+var once sync.Once
 
-	logger.SetFormatter(formatter)
-	return &Logger{logger}
-}
+// Log is
+func Log() *logger {
+	once.Do(func() {
+		log := logrus.New()
+		formatter := &logrus.TextFormatter{
+			FullTimestamp: true,
+		}
+		log.SetFormatter(formatter)
 
-// SetLevel set logger instance. Could be Trace,
-// Debug, Info, Warning, Error, Fatal and Panic
-func (l *Logger) SetLevel(level string) error {
-	parsed, err := logrus.ParseLevel(level)
-	if err != nil {
-		return fmt.Errorf("parse log level from string: %w", err)
-	}
-
-	l.Logger.SetLevel(parsed)
-	return nil
+		instance = &logger{log}
+	})
+	return instance
 }
